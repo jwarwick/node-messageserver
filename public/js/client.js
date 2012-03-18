e84793065e9b2d806b5ffe83badf6dce418a993e
John Warwick <jwarwick@gmail.com>
2012-03-17 21:34:07 -0400

    Added smoothie.js for charting

--- public/js/client.js
+++ public/js/client.js
@@ -14,6 +14,23 @@ jQuery(document).ready(function($) {
         .appendTo('#messages')
         .fadeIn();
     });
-    
   });
+  
+  
+  self.smoothie = new SmoothieChart();
+  self.smoothie.streamTo(document.getElementById("my_canvas"), 1000);
+  
+  self.line1 = new TimeSeries();
+  self.line2 = new TimeSeries();
+  
+  setInterval(function() {
+    self.line1.append(new Date().getTime(), Math.random());
+    self.line2.append(new Date().getTime(), Math.random());
+  }, 1000);
+  
+  self.smoothie.addTimeSeries(self.line1,
+    { strokeStyle:'rgb(0, 255, 0)', fillStyle:'rgba(0, 255, 0, 0.4)', lineWidth:3 });
+  self.smoothie.addTimeSeries(self.line2, 
+    { strokeStyle:'rgb(255, 0, 255)', fillStyle:'rgba(255, 0, 255, 0.3)', lineWidth:3 });
+  
 });
